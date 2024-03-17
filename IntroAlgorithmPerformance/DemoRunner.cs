8e1217658a8c750dbd944c163a8d4f1954c81bc5
GET Academy <terje@getacademy.no>
2024-03-17 14:50:18 +0100

    Create DemoRunner.cs

---
+++ IntroAlgorithmPerformance/DemoRunner.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace IntroAlgorithmPerformance
+{
+    internal class DemoRunner
+    {
+        public static void Run(Action<int> action, params int[] values)
+        {
+            var stopwatch = new Stopwatch();
+
+            foreach (var value in values)
+            {
+                Console.WriteLine($"Start N={value}");
+                stopwatch.Start();
+                action(value);
+                stopwatch.Stop();
+                var spent = stopwatch.ElapsedMilliseconds / 1000.0;
+                Console.WriteLine($"Brukte {spent:F} sekunder");
+            }
+        }
+    }
+}
